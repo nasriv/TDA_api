b9c663395dc1f64a2a8c1e28e2007dad7251926f
nasriv <vnasri91@gmail.com>
2021-12-08 00:35:03 -0500

    added input modifiers

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 @app.route('/', methods=["POST","GET"])
 def index():
     symbol = request.form.get('symbol')
+    periodType = request.form.get('periodType')
+    period = request.form.get('period')
     try:
-        data = callTest(symbol)
+        resp = callTest(symbol, periodType, period)
     except:
-        data = ""
+        resp = ""
+    data = { "chartData":resp,
+            "symbol":symbol }
     return render_template("index.html", data=data)
--- functions.py
+++ functions.py
@@ -2,12 +2,12 @@ from client_id import client_id
 import requests
 import pandas as pd
 
-def callTest(symbol):
+def callTest(symbol, periodType, period):
     url = 'https://api.tdameritrade.com/v1/marketdata/{}/pricehistory'.format(symbol)
     payload = {
         'apikey':client_id,
-        'periodType':'year',
-        'period':'1',
+        'periodType':str(periodType),
+        'period': str(period),
         'frequencyType':'daily',
         'frequency':'1',
         # 'startDate':str(datetime.datetime(2020,1,1,0,0).timestamp()).split('.')[0],
